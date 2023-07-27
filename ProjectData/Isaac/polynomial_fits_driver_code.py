213b15ead1cb07b5ff3a1eb7640c1b4d40bd5efd
Is Ding <2431261252@qq.com>
2023-07-27 02:35:41 -0700

    added fits with augmented canada's covid cases

--- ProjectData/Isaac/polynomial_fits_driver_code.py
+++ ProjectData/Isaac/polynomial_fits_driver_code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import unemployment_rate_fit as usual
 import during_pandemic_fit as during_pandemic
+import augmented_fit as augmented
 import os
 names = ['Alberta',
          'British Columbia',
@@ -16,12 +17,16 @@ names = ['Alberta',
 ]
 os.makedirs('./usual', exist_ok = True)
 os.makedirs('./pandemic', exist_ok = True)
+os.makedirs('./augmented', exist_ok = True)
 usual_results_df = pd.DataFrame(columns=['GEO',
                                          'result 1 pvalue',
                                          'result 2 pvalue',
                                          'result 3 score',
                                          'result 4 score'])
 during_pandemic_df = usual_results_df
+augmented_df = pd.DataFrame(columns = ['GEO',
+                                       'result 1 score',
+                                       'result 2 score'])
 for name in names:
     usual_result = usual.fit(name)
     usual_results_df = pd.concat([usual_results_df, usual_result],
@@ -31,7 +36,11 @@ for name in names:
     during_pandemic_df = pd.concat([during_pandemic_df, during_pandemic_result],
                                    axis = 0,
                                    ignore_index = True)
+    augmented_result = augmented.fit(name)
+    augmented_df = pd.concat([augmented_df, augmented_result],
+                             axis = 0,
+                             ignore_index = True)
 
 usual_results_df.to_csv('usual_polynomial_fits.csv', index = False)
 during_pandemic_df.to_csv('during_pandemic_polynomial_fits.csv', index = False)
-
+augmented_df.to_csv('augmented_fits.csv', index = False)
