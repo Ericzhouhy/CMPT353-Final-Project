a0fcf759c1d6df71f920d8b19cce8abc4e840b7c
Is Ding <2431261252@qq.com>
2023-07-26 19:53:49 -0700

    added linear regression tests

--- ProjectData/Isaac/unemployment_rate_fit.py
+++ ProjectData/Isaac/unemployment_rate_fit.py
@@ -1,2 +1,10 @@
 import pandas as pd
-from sklearn.
+from scipy.stats import linregress
+
+data = pd.read_csv('../covid_and_employment_total_included.csv')
+
+result = linregress(data[['total unemployment rate(%)', 'covid cases']])
+result_2 = linregress(data[['total unemployment rate(%)', 'covid deaths']])
+
+print(f'the p value for linear regression of unemployment rate and covid cases is {result.pvalue}')
+print(f'the p value for linear regression of unemployment rate and covid deaths is {result_2.pvalue}')
