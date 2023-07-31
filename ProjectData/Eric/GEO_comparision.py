993d8f0eec55200e249ae37297cdb825c4f7f902
ericzhouhy <iamericzhou@gmail.com>
2023-07-31 12:44:23 -0700

    Clean the python code

--- ProjectData/Eric/GEO_comparision.py
+++ ProjectData/Eric/GEO_comparision.py
@@ -7,25 +7,26 @@ from scipy.stats import mannwhitneyu
 df = pd.read_csv('unempl_both_sex.csv')
 
 plt.figure(figsize=(12, 6))
+# Keep only two provinces
 new_df = df[df['GEO'].isin(['Newfoundland and Labrador', 'Saskatchewan'])]
 
+# Create the plot
 sns.lineplot(x='REF_DATE', y='Unemployment Rate', hue='GEO', data=new_df)
 
 plt.title('Unemployment Rate in Newfoundland and Labrador and Saskatchewan')
 plt.xlabel('Date')
 plt.ylabel('Unemployment Rate')
 
-# Rotate x-axis labels for better readability
 plt.xticks(rotation=45)
 
 ax = plt.gca()
 ax.xaxis.set_major_locator(MaxNLocator(nbins=8)) 
 
-# Show the plot
 plt.legend()
 plt.tight_layout()
 plt.show()
 
+#Print the p-value
 Newfoundland = new_df[new_df['GEO']=='Newfoundland and Labrador']['Unemployment Rate']
 Saskatchewan = new_df[new_df['GEO']=='Saskatchewan']['Unemployment Rate']
 statistic, pvalue = mannwhitneyu(Newfoundland, Saskatchewan, alternative='two-sided')
